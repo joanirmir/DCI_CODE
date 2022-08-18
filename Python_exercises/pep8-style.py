59bcf5eb49451d49b3fdb7796385d0aaa28b0b43
joanirmir <joanirmir@gmail.com>
2022-08-18 14:24:39 +0200

    pep8

---
+++ Python_exercises/pep8-style.py
@@ -0,0 +1,39 @@
+# What is pep8?
+# Coding conventions for the Python code comprising the standard library in the main Python distribution
+
+
+# Example 1
+'''
+VariableName = 4 * 6 + 7* 9
+print(VariableName)
+'''
+
+a=5
+b= 7
+d = 3
+e=4
+
+C = (a + b) - (d - e)
+
+'''
+# Example 2
+
+O = 4
+I = 2
+
+if O == 4 : print(O, I) ; O, I = I , O
+
+
+
+# Example 3
+
+y= 15
+
+result = y+ 8*9
+
+if result>=20 : 
+        print("Hello")
+else:
+         print('World')
+             
+'''
