68e6904c5625e6a39911791fe7fe1c09d1502af4
joanirmir <joanirmir@gmail.com>
2022-08-24 14:11:41 +0200

    Strings

---
+++ Python_exercises/String_Basics_1/ex2/String_Basics2.py
@@ -0,0 +1,33 @@
+"""# Task 1
+
+# Get the length of a variable with len()
+
+text = "Berlin is a world city of culture, politics, media and science."
+
+len(text) 
+
+# Task 2
+
+# To get the first character of a variable use index [0]
+
+text[0]
+
+# To get the last character of a variable use index [-1]
+
+text[-1]
+
+# Task 3
+
+# Using the upper() in-built function makes every character upper
+
+text[:3].upper()
+
+"""
+
+# Task 4
+
+text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital"
+
+# Using the find() function
+
+print("B appears: " + str(text.count("B")) + " times")
