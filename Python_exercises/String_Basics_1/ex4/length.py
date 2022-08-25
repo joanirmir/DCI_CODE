ad8de362c8e9d16f675776496d52597b6a44daaa
joanirmir <joanirmir@gmail.com>
2022-08-25 09:55:14 +0200

    str

--- Python_exercises/String_Basics_1/ex4/length.py
+++ Python_exercises/String_Basics_1/ex4/length.py
@@ -1,9 +1,11 @@
-word = input("Enter a word: ")
+# How to check if the length of a word is odd or even
+
+word = str(input("Enter a word: "))
 
 if len(word)%2 == 0:
-    print("The string has an even number of characters.")
+    print(f"The string '{word}' has an even number of characters.")
 elif len(word)%2 != 0:
-    print("The string has an odd number of characters.")
+    print(f"The string '{word}' has an odd number of characters.")
 elif len(word) == "":
     print("The string has an even number of characters.")
 else:
