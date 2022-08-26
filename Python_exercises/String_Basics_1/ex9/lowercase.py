e13395b348905fcf208359907e9ad9615b02bd5a
joanirmir <joanirmir@gmail.com>
2022-08-26 10:54:01 +0200

    ex9

---
+++ Python_exercises/String_Basics_1/ex9/lowercase.py
@@ -0,0 +1,31 @@
+# Define a function to lowercase a string
+
+# Defining user input
+
+sentence = input("Enter a sentence: ")
+
+def normalize(sentence):
+    if sentence.isupper():
+        sentence_new = sentence[0].capitalize() + sentence[1:].lower() + "!"
+        print(sentence_new)
+    else:
+        print("try again!")
+
+normalize(sentence)
+
+"""
+# How to convert uppercase to lowercase with for loop
+
+s = input("Enter a sentence: ")
+new_str=""
+for i in range (len(s)):
+    if s[i].isupper():
+        new_str+=[i].lower() # lower() not works for lists
+    elif s[i].islower():
+        new_str+=s[i].upper()
+    else:
+        new_str+=s[i]
+print(new_str)
+"""
+
+
