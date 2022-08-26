90f7e188c2319b6f1c49bac58b748aa2588762e8
joanirmir <joanirmir@gmail.com>
2022-08-26 11:30:19 +0200

    new

--- Python_exercises/String_Basics_1/ex11/replacing.py
+++ Python_exercises/String_Basics_1/ex11/replacing.py
@@ -1,9 +1,25 @@
-# Replacing a substring in a string
+# Replacing a substring in a string if you just want to replace a single word with regex
 
-import re
+# Importing re
 
-story = input("Enter the story: ")
+"""import re
+
+story = "A dogmatic dog buys dogecoin to become rich and buy hotdogs every day."
+
+# Using the substring function sub()
 
 new_string = (re.sub(r"\bdog\b", "cat", story))
 
-print(new_string)
+print(new_string) """
+
+# Different way
+
+text = "A dogmatic dog buys dogecoin to become rich and buy hotdogs every day."
+
+word = text.replace(" dog ", " cat ")
+print(word)
+
+new = text.replace(" dog ", " cat ")
+print(f"{new}")
+
+print(f"{text.replace(" dogs ", " cat ")}")
