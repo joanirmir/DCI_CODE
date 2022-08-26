3222f4e628f6148eeebe4f69afa14b2835bf1dfa
joanirmir <joanirmir@gmail.com>
2022-08-26 13:35:49 +0200

    n

--- Python_exercises/String_Basics_1/ex10/shoppinglist.py
+++ Python_exercises/String_Basics_1/ex10/shoppinglist.py
@@ -1,7 +1,9 @@
-# User input
 
-#shopping_list = input("Enter your groceries: ")
+# Join all items in a tuple into a string, using a comma character as separator:
 
 my_shopping_list = ("bananas", "apples", "milk")
 
 new_list = ",".join(my_shopping_list)
+
+print(f'"{new_list}"')
+
