bb03c99ca4a071732b9e1419ff2daa27542a2bce
joanirmir <joanirmir@gmail.com>
2022-08-31 11:39:19 +0200

    ex1 changes

---
+++ Python_exercises/String_Basics_1/ex1/ex1.py
@@ -0,0 +1,25 @@
+"""# Task 1
+
+city = "London"
+print(city)
+
+# Task 2
+
+city = "berlin"  
+population = 3645000
+
+print(city.capitalize() + ":" + str(population))
+
+# Task 3
+
+city = "london"
+population = "9000000"
+
+print("City: ", city.capitalize(), string.isalpha(city))"""
+
+# Task 4
+
+text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+
+print("capital: " text.index("capital"))
+
