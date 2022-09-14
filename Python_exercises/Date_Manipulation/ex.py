1affcbf862d62e2d4966aef98feb0b7244ad6458
joanirmir <joanirmir@gmail.com>
2022-09-14 11:44:25 +0200

    Sleep function

---
+++ Python_exercises/Date_Manipulation/ex.py
@@ -0,0 +1,20 @@
+# Import time
+
+from time import sleep
+
+# Using sleep function
+
+sleep(1)
+print("###### This ######")
+sleep(1)
+print("###### IS ######")
+sleep(1)
+print("##### SOME ######")
+sleep(1)
+print("###### DOPE ######")
+sleep(1)
+print("###### EXAMPLE ######")
+sleep(1)
+print("###### INNIT ######")
+sleep(1)
+print("###### ???? ######")
