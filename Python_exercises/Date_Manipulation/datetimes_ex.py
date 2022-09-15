461f778495ec8358b59b6e72ca2bdec82d0c724e
joanirmir <joanirmir@gmail.com>
2022-09-15 09:45:40 +0200

    Datetime Module

---
+++ Python_exercises/Date_Manipulation/datetimes_ex.py
@@ -0,0 +1,29 @@
+# Datetime Modules (Dates and Times)
+
+# Importing datetime
+
+import datetime
+
+# Use dir() to see all the modules of the library
+
+dir(datetime)
+
+# Use help() for more information
+
+help(datetime.date)
+
+# datetime.date() takes three arguments - year, month, day
+
+gvr = datetime.date(1956, 1, 31)
+
+# Access one argument via variable.month/ variable.day / variable.year
+
+print(gvr)
+print(gvr.month)
+print(gvr.year)
+print(gvr.day)
+
+# How to use datetime.timedelta()
+
+
+
