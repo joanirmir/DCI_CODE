5b66d2d476d55581d688cd1e2d3f2eea57944f11
joanirmir <joanirmir@gmail.com>
2022-09-22 09:22:52 +0200

    Tansforming datetype to string

--- Python_exercises/Date_Manipulation/task2/ex3.py
+++ Python_exercises/Date_Manipulation/task2/ex3.py
@@ -2,13 +2,15 @@ import datetime
 
 start_date = datetime.datetime(year=2021, month=1, day=25)
 
+new_date = start_date.strftime("%d %B, %Y")
+
 print(start_date)
+print(new_date)
 
 print(type(start_date))
 
-message = f("Hello Friedrich, your rent of 300€ is due on {datetime.strptime(start_date, "%M %A, %Y")}")
+message = f"Hello Friedrich, your rent of 300€ is due on {new_date}."
+
+print(message)
 
 
-user_input = input("Enter ")
-
-start_date = datetime(year=2020, month=1, day=1)
