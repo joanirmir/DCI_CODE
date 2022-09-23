fad62fab04847c74babc21eb136835048d8f779e
joanirmir <joanirmir@gmail.com>
2022-09-23 16:05:40 +0200

    Args

--- Python_exercises/function_python/function_greeting.py
+++ Python_exercises/function_python/function_greeting.py
@@ -4,10 +4,22 @@
 """def full_name(first_name="John/Jane", last_name="Doe"):
     return f"Good morning {first_name} {last_name}"
 
-print(full_name(last_name="Peter"))"""
+print(full_name(last_name="Peter"))
 
 # How to use *args 
 def full_name(*args):
     return f"{args[0]} {args[1]} {args[2]} {args[3]}"
 
-print(full_name("Felipe", "Gonzales", "Pedro", "Alfonso II"))
+print(full_name("Felipe", "Gonzales", "Pedro", "Alfonso II"))"""
+
+# Defining a function to use args to concatenate several names to one string
+
+def full_name(*args): # "tuple"
+    full_string = ""
+    
+    for arg in args:
+        full_string += f"{arg} "
+
+    print(full_string)
+
+full_name("A", "B", "C")
