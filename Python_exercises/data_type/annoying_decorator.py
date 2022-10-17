aa84ac2c59832f6a4d6fc32d44c01c6a0969c247
joanirmir <joanirmir@gmail.com>
2022-10-17 14:11:35 +0200

    New decorator

--- Python_exercises/data_type/annoying_decorator.py
+++ Python_exercises/data_type/annoying_decorator.py
@@ -15,3 +15,17 @@ def sum(x, y):
     return x + y
 
 print(sum(3,4))
+
+# Not so annoying decorator
+
+def not_so_annoying_decorator(f):
+    def decorated_function(*args, **kwargs):
+        print('Hey...I will execute your thing...')
+        f(*args, **kwargs)
+    return decorated_function
+
+@not_so_annoying_decorator
+def greeting(name):
+    print(f'hello {name}')
+
+greeting('matias')
