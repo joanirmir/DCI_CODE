98f1089f3143764a431382bed4ff031949f8b999
joanirmir <joanirmir@gmail.com>
2022-10-21 15:24:42 +0200

    Reversed while loop

--- Python_exercises/data_type/collections/strings.py
+++ Python_exercises/data_type/collections/strings.py
@@ -6,7 +6,7 @@
 
 #print(fruit[i+1])
 
-# len is a built-in function that returns the number of characters in a string:
+"""# len is a built-in function that returns the number of characters in a string:
 fruit = 'banana'
 print(len(fruit))
 
@@ -14,3 +14,21 @@ length = len(fruit)
 last = fruit[length-1]
 print(last)
 
+# Traversal with a for loop
+
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(letter)
+    index = index + 1
+
+# Write a function that takes a string as an argument and displays the letters backward, one per line:
+
+def letters_backward(string):
+    index = (len(string)-1)
+    while index >= 0:
+        letter = string[(index)]
+        print(letter)
+        index = index - 1
+
+print(letters_backward('banana'))"""
