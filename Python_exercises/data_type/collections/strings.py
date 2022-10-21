56e8ad9d11d17d9b2b4e5f9b37138dfd2b9549b7
joanirmir <joanirmir@gmail.com>
2022-10-21 15:04:50 +0200

    ThinkPyhton_Chapter Stings

---
+++ Python_exercises/data_type/collections/strings.py
@@ -0,0 +1,16 @@
+# ThinkPython - Strings
+
+
+#i = 1
+#fruit = 'banana'
+
+#print(fruit[i+1])
+
+# len is a built-in function that returns the number of characters in a string:
+fruit = 'banana'
+print(len(fruit))
+
+length = len(fruit)
+last = fruit[length-1]
+print(last)
+
