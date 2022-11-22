7accdf63762f707555d7f5c3ebc9b10e2a1b3d51
joanirmir <joanirmir@gmail.com>
2022-11-22 16:11:39 +0100

    Calculating_age_of_user

---
+++ Pytest_tutorial/test_calculate_age.py
@@ -0,0 +1,9 @@
+from calculate_age import get_age
+
+def test_get_age():
+    # Given.
+    yyyy, mm, dd = map(int, "1996/07/11".split('/'))   
+    # When.
+    age = get_age(yyyy, mm, dd)
+    # Then.
+    assert age == 26 
